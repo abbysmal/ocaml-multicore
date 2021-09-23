4754ed8b33bb897bd5427ce21899e2136e005344
Enguerrand Decorne <decorne.en@gmail.com>
2021-09-23 18:27:42 +0200

    ci: try upload_last_backtrace

--- tools/ci/actions/backtrace_extractor.sh
+++ tools/ci/actions/backtrace_extractor.sh
@@ -1,4 +1,4 @@
 while true; do
-    ps axf | grep ocaml | grep -v grep | awk '{ system("sudo gdb -ex \"set pagination 0\" -ex \"thread apply all bt\" -batch /proc/"$1"/exe -p "$1" 2> /dev/null")}' > /tmp/last_backtrace.txt
+    ps axf | grep ocaml | grep -v grep | awk '{ system("sudo gdb -ex \"set pagination 0\" -ex \"thread apply all bt\" -batch /proc/"$1"/exe -p "$1" 2> /dev/null")}' >> /tmp/last_backtrace.txt
     sleep 60
 done
