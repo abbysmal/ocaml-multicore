640ed39b4204d6c3ce55936480d6ff6b4a9e29a6
Enguerrand Decorne <decorne.en@gmail.com>
2021-09-23 17:40:26 +0200

    ci: try upload_last_backtrace

---
+++ tools/ci/actions/backtrace_extractor.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+
+while true; do
+    ps -e -o "%p," -o "%a" | grep ocaml | grep -v grep | awk -F , '{ system("date +\"%T\" >> /tmp/last_backtrace.txt; echo \""$2"\" >> /tmp/last_backtrace.txt; sudo gdb -ex \"source tools/ci/actions/gdb_commands\" -batch /proc/"$1"/exe -p "$1" 2> /dev/null")}' >> /tmp/last_backtrace.txt
+    sleep 140
+done
