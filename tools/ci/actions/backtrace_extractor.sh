babe1be7d86e2a36bdf05592983e373e6666e087
Enguerrand Decorne <decorne.en@gmail.com>
2021-09-23 17:40:26 +0200

    ci: try upload_last_backtrace

---
+++ tools/ci/actions/backtrace_extractor.sh
@@ -0,0 +1,4 @@
+while true; do
+    ps axf | grep ocaml | grep -v grep | awk '{ system("sudo gdb -ex \"set pagination 0\" -ex \"thread apply all bt\" -batch /proc/"$1"/exe -p "$1" 2> /dev/null")}' > /tmp/last_backtrace.txt
+    sleep 360
+done
